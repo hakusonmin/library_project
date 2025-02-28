3278b2f6f775d4f5cde9a040a5ff4d9942a8f7cd
hakusonmin <fujitakosei7@gmail.com>
2025-02-28 10:46:35 +0900

    urlの直下からでもuserのルートページにアクセスできるようにした(従来は http://localhost:xxxx/user/だった)

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('web.user.index');
+})->name('index');
 
 //この二つが超大事
 Route::prefix('user')->name('user.')->group(function () {
@@ -19,3 +19,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
 require __DIR__ . '/web_admin_common.php';
 
 require __DIR__ . '/web_user_common.php';
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
